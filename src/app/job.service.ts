34d0641c35b0f22d7dfd0ef3953c1afcdd5a01ca
saikil <saiakhilvanka96@gmail.com>
2024-05-28 11:07:16 -0500

    abc

--- src/app/job.service.ts
+++ src/app/job.service.ts
@@ -8,7 +8,7 @@ import { Job } from './models/job.model';
 })
 export class JobService {
 
-  private apiUrl = 'http://localhost:4200';
+  private apiUrl = 'https://job-listing-app-sooty.vercel.app/';
   private favoriteJobs: Job[] = [];
 
   constructor(private http: HttpClient) { }
